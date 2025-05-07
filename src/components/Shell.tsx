5e664e583a9cf14ec172d199bc83c5217ee9dc40
Nicolas Frouin <frouinnicolas@gmail.com>
2025-05-07 10:57:32 +0200

    add logo and center it

--- src/components/Shell.tsx
+++ src/components/Shell.tsx
@@ -1,10 +1,11 @@
 'use client';
 
-import { AppShell, Burger, Group } from '@mantine/core';
+import { AppShell, AspectRatio, Burger, Group } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import Nav from './Nav';
 import { HEADER_HEIGHT } from '@/utils/constants';
 import Link from 'next/link';
+import Image from 'next/image';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 interface Props extends React.PropsWithChildren {}
@@ -22,14 +23,27 @@ export default function Shell({ children }: Props) {
       <AppShell.Header>
         <Group h={'100%'} px={'md'}>
           <Burger opened={opened} onClick={toggle} hiddenFrom={'sm'} size={'sm'} />
-          <Group justify={'space-between'} align={'center'} style={{ flex: 1 }}>
-            <Link href={'/'}>
-              <div>Logo</div>
+          <Group
+            align={'center'}
+            style={{ flex: 1 }}
+            className={'!justify-center sm:!justify-between'}
+          >
+            <Link href={'/'} className={'flex items-center'}>
+              <AspectRatio ratio={815 / 161}>
+                <Image
+                  src={'/logo/logo_aphp.png'}
+                  alt={'Logo APHP'}
+                  width={815}
+                  height={161}
+                  className='h-12'
+                />
+              </AspectRatio>
             </Link>
             <Group ml={'xl'} gap={0} visibleFrom={'sm'}>
               <Nav />
             </Group>
           </Group>
+          <div className='w-7 sm:hidden' />
         </Group>
       </AppShell.Header>
       <AppShell.Navbar py={'md'} px={4} w={'100vw'} hiddenFrom={'sm'}>
