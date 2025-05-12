8f3ff3f94e83d887a48ce4e6d0b26e636265a00e
Nicolas Frouin <frouinnicolas@gmail.com>
2025-05-12 17:28:50 +0200

    fix types for mission data

--- src/actions/data.ts
+++ src/actions/data.ts
@@ -1,11 +1,14 @@
 'use server';
 
 import prisma from '@/prisma';
+import { BSM } from '@/types/utils';
 import { getDate, getWeekDays } from '@/utils/date';
 
-export async function getBranchesToMissions() {
+export async function getBranchesToMissions(): Promise<BSM[]> {
   return await prisma.branch.findMany({
-    include: { sectors: { include: { missions: { where: { active: true } } }, where: { active: true } } },
+    include: {
+      sectors: { include: { missions: { where: { active: true } } }, where: { active: true } },
+    },
     where: { active: true },
   });
 }
@@ -14,12 +17,7 @@ export async function getWeekAssignmentsData(date: Date = new Date()) {
   const days = getWeekDays(date);
 
   return await prisma.assignment.findMany({
-    where: {
-      date: { in: days.map(getDate) },
-    },
-    include: {
-      caregiver: true,
-      mission: true,
-    },
+    where: { date: { in: days.map(getDate) } },
+    include: { caregiver: true, mission: true },
   });
 }
--- src/types/utils.ts
+++ src/types/utils.ts
@@ -1,5 +1,11 @@
+import { Branch, Mission, Sector } from '@/prisma/generated/client';
+
 type NotRequired = 'id' | 'color' | 'active' | 'createdAt' | 'updatedAt' | `${string}Id`;
 
 export type NonRequired<T, O extends keyof T = never> = {
   [K in keyof T as K extends NotRequired | O ? never : K]-?: T[K];
 } & Partial<Pick<T, Extract<keyof T, NotRequired | O>>>;
+
+export type BranchesSectorsMissions = Branch & { sectors: (Sector & { missions: Mission[] })[] };
+/** @alias {@link BranchesSectorsMissions} */
+export type BSM = BranchesSectorsMissions;
