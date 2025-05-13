cd181c6350f4fbaefe2bf27a7c26399816a4f95b
Nicolas Frouin <frouinnicolas@gmail.com>
2025-05-13 10:21:59 +0200

    add first step of planner

--- src/app/planificateur/page.tsx
+++ src/app/planificateur/page.tsx
@@ -1,7 +1,50 @@
+'use client';
+
+import CaregiverOptions from '@/components/planner/CaregiverOptions';
+import { Caregiver, Sector } from '@/prisma/generated/client';
+import { Button, Group, Stepper } from '@mantine/core';
+import { useState } from 'react';
+
+interface StepProps {
+  label: string;
+  component: React.ReactNode;
+}
+
 export default function Page() {
+  const [active, setActive] = useState(0);
+
+  const [forbiddenSectors, setForbiddenSectors] = useState<Record<Caregiver['id'], Sector['id'][]>>(
+    {},
+  );
+
+  const STEPS: StepProps[] = [
+    {
+      label: 'Choix des secteurs à ne pas attribuer',
+      component: (
+        <CaregiverOptions
+          forbiddenSectors={forbiddenSectors}
+          setForbiddenSectors={setForbiddenSectors}
+        />
+      ),
+    },
+  ];
+
+  const nextStep = () => setActive((prev) => (prev < STEPS.length ? prev + 1 : prev));
+  const prevStep = () => setActive((prev) => (prev > 0 ? prev - 1 : prev));
+
   return (
     <div>
-      <h1>PLanificateur</h1>
+      <Stepper active={active} onStepClick={setActive} allowNextStepsSelect={false}>
+        {STEPS.map((step, i) => (
+          <Stepper.Step key={i} label={step.label}>
+            {step.component}
+          </Stepper.Step>
+        ))}
+      </Stepper>
+      <Group justify={'center'} mt='xl'>
+        <Button onClick={prevStep}>Précédent</Button>
+        <Button onClick={nextStep}>Suivant</Button>
+      </Group>
     </div>
   );
 }
---
+++ src/components/planner/CaregiverOptions.tsx
@@ -0,0 +1,120 @@
+'use client';
+
+import { fetchCaregiver } from '@/actions/common';
+import { getBranchesToMissions } from '@/actions/data';
+import { Branch, Caregiver, Sector } from '@/prisma/generated/client';
+import { BSM } from '@/types/utils';
+import { Box, Chip, Flex, Group, LoadingOverlay, Pagination, Table, Text } from '@mantine/core';
+import { useEffect, useState } from 'react';
+import { FaTimes } from 'react-icons/fa';
+
+interface CaregiversAndBranch extends Caregiver {
+  branch: Branch;
+}
+
+interface Props {
+  forbiddenSectors: Record<Caregiver['id'], Sector['id'][]>;
+  setForbiddenSectors: React.Dispatch<
+    React.SetStateAction<Record<Caregiver['id'], Sector['id'][]>>
+  >;
+}
+
+export default function CaregiverOptions({ forbiddenSectors, setForbiddenSectors }: Props) {
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  // eslint-disable-next-line
+  const [pageSize, setPageSize] = useState(10);
+  const [total, setTotal] = useState(0);
+  const [caregivers, setCaregivers] = useState<CaregiversAndBranch[]>([]);
+  const [data, setData] = useState<BSM[]>([]);
+
+  const tableHeaders: React.ReactNode[] = [
+    <Text key={'h-sectors'} ta={'center'} fw={'bold'}>
+      Nom
+    </Text>,
+    <Text key={'h-sectors'} ta={'center'} fw={'bold'}>
+      Secteurs à ne pas attribuer
+    </Text>,
+  ];
+  const tableBody: React.ReactNode[][] = caregivers.map((c) => {
+    const branch = data.find((b) => b.id === c.branchId);
+    return [
+      <Text key={`${c.id}-name`}>{[c.firstname, c.lastname].join(' ')}</Text>,
+      branch ? (
+        <Chip.Group
+          key={`${c.id}-sectors`}
+          multiple
+          value={forbiddenSectors[c.id]}
+          onChange={(value) => {
+            setForbiddenSectors((prev) => ({ ...prev, [c.id]: value }));
+          }}
+        >
+          <Flex justify={'center'} align={'center'} gap={'xs'} className={'flex-col md:flex-row'}>
+            {branch.sectors.map((s) => (
+              <Chip
+                key={`${c.id}-${s.id}`}
+                value={s.id}
+                color={'red'}
+                icon={<FaTimes />}
+                styles={{ label: { border: `2px solid ${s.color || '#ddd'}` } }}
+              >
+                {s.name}
+              </Chip>
+            ))}
+          </Flex>
+        </Chip.Group>
+      ) : (
+        <Text key={`${c.id}-no-branch`}>
+          Ce soignant est affecté à une branche introuvable ou inactive.
+        </Text>
+      ),
+    ];
+  });
+
+  useEffect(() => {
+    async function fetchData() {
+      setLoading(true);
+      const totalData = await fetchCaregiver('count', [{ where: { active: true } }]);
+      const caregiversData = await fetchCaregiver('findMany', [
+        {
+          where: { active: true },
+          include: { branch: true },
+          take: pageSize,
+          skip: (page - 1) * pageSize,
+          orderBy: [{ lastname: 'asc' }, { firstname: 'asc' }],
+        },
+      ]);
+      const allData = await getBranchesToMissions();
+
+      setTotal(totalData);
+      setCaregivers(caregiversData);
+      setData(allData);
+      setLoading(false);
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+    fetchData();
+  }, [page, pageSize]);
+
+  return (
+    <div>
+      <Flex direction={'column'} gap={'md'}>
+        <Box pos={'relative'}>
+          <LoadingOverlay
+            visible={loading}
+            zIndex={1000}
+            overlayProps={{ radius: 'sm', blur: 2 }}
+          />
+          <Table data={{ head: tableHeaders, body: tableBody }} highlightOnHover />
+        </Box>
+        <Group justify={'center'} mt={'md'}>
+          <Pagination
+            total={Math.ceil(total / pageSize)}
+            value={page}
+            onChange={setPage}
+            siblings={2}
+          />
+        </Group>
+      </Flex>
+    </div>
+  );
+}
