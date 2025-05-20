8e5b85b57ca4aa54cd80dfb4fbaa61425909736e
Nicolas Frouin <frouinnicolas@gmail.com>
2025-05-20 15:29:25 +0200

    refactor prisma.ts to enhance query handling and add archived logic for operations

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,52 @@
-import { PrismaClient } from '@/generated/client';
+import { PrismaClient, PrismaPromise } from '@/generated/client';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+type PrismaExcludedKeys = `$${string}` | keyof object | symbol;
+type PrismaModelNames = Exclude<keyof PrismaClient, PrismaExcludedKeys>;
+type PrismaModelsQueries = Exclude<keyof PrismaClient[PrismaModelNames], PrismaExcludedKeys>;
 
 const prisma =
   globalForPrisma.prisma ||
   new PrismaClient().$extends({
     query: {
-      $allOperations: async ({ args, operation, query, model }) => {
+      $allOperations: async ({
+        args,
+        operation,
+        query,
+        model,
+      }): Promise<PrismaPromise<unknown>> => {
         if (process.env.NODE_ENV !== 'production') {
           console.log(`[PRISMA] ${model}::${operation}`);
+          console.log(`[PRISMA] Args before : ${JSON.stringify(args)}`);
+        }
+
+        const ogOperation = operation;
+
+        switch (operation) {
+          case 'findUnique':
+          case 'findFirst':
+          case 'findUniqueOrThrow':
+          case 'findFirstOrThrow':
+          case 'findMany':
+          case 'update':
+          case 'updateMany':
+            if (args?.where?.archived === undefined) {
+              args = { ...(args || {}), where: { ...(args.where || {}), archived: false } };
+            }
+            break;
+          case 'delete':
+          case 'deleteMany':
+            operation = operation === 'delete' ? 'update' : 'updateMany';
+            args = { ...(args || {}), data: { ...(args.data || {}), archived: true } };
+            break;
+        }
+        if (process.env.NODE_ENV !== 'production') {
+          console.log(`[PRISMA] Args after : ${JSON.stringify(args)}`);
+        }
+        if (operation !== ogOperation) {
+          // @ts-expect-error ---
+          return await prisma[model as PrismaModelNames][operation as PrismaModelsQueries](args);
         }
         return await query(args);
       },
