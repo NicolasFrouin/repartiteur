b66592dc64a2ce49633e4989a1cf04a0ecb9c1e1
Nicolas Frouin <frouinnicolas@gmail.com>
2025-06-16 11:54:34 +0200

    feat: add metadata for NotFound page with title and description

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,4 +1,10 @@
 import NotFound from '@/components/error/NotFound';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Page introuvable',
+  description: "La page que vous recherchez n'existe pas ou a été déplacée.",
+};
 
 export default function NotFoundPage() {
   return <NotFound />;
