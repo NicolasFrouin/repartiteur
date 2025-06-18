55e9f16d176ddb2b31f13bb5f7cd03107068116f
Nicolas Frouin <frouinnicolas@gmail.com>
2025-06-18 15:35:51 +0200

    refactor: remove debounce function from utils.ts to streamline code

--- src/lib/utils/utils.ts
+++ src/lib/utils/utils.ts
@@ -6,18 +6,6 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function debounce<T extends (...args: any[]) => any>(func: T, delay: number = 300) {
-  let timeout: NodeJS.Timeout;
-  return function (...args: Parameters<T>) {
-    // @ts-expect-error ---
-    // eslint-disable-next-line @typescript-eslint/no-this-alias
-    const context = this;
-    clearTimeout(timeout);
-    timeout = setTimeout(() => func.apply(context, args), delay);
-  };
-}
-
 export function toSlug(str: string = ''): string {
   return slugify(str, {
     locale: 'fr',
