611d9b69b2e668e15d5c9d4a96c46208e9e9506d
Nicolas Frouin <frouinnicolas@gmail.com>
2025-07-02 10:36:17 +0200

    refactor: enhance type definitions for FullCaregiver, FullAssignment, FullSector, FullMission, and FullCaregiverSector

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -26,27 +26,64 @@ export type BranchesSectorsMissions = Branch & { sectors: (Sector & { missions: 
 /** @alias {@link BranchesSectorsMissions} */
 export type BSM = BranchesSectorsMissions;
 
+/**
+ * A `FullCaregiver` is a `Caregiver` with :
+ * - `branch` : {@link Branch} the branch the caregiver belongs to
+ * - `assignedSectors` : {@link FullCaregiverSector[]} the sectors assigned to the caregiver
+ * - `updatedBy` (optional) : {@link User} the user who updated the caregiver
+ */
 export type FullCaregiver = Caregiver & {
   branch: Branch;
   assignedSectors: FullCaregiverSector[];
   updatedBy?: User | null;
 };
+
+/**
+ * A `FullAssignment` is an `Assignment` with :
+ * - `caregiver` : {@link Caregiver} the caregiver assigned to the mission
+ * - `mission` : {@link Mission} the mission assigned to the caregiver
+ * - `updatedBy` (optional) : {@link User} the user who updated the assignment
+ */
 export type FullAssignment = Assignment & {
   caregiver: Caregiver;
   mission: Mission;
   updatedBy?: User | null;
 };
+
+/**
+ * A `FullCaregiverSector` is a `CaregiverSector` with :
+ * - `caregiver` (optional) : {@link Caregiver} the caregiver assigned to the sector
+ * - `sector` (optional) : {@link Sector} the sector assigned to the caregiver
+ */
 export type FullBranch = Branch & { sectors: Sector[]; updatedBy?: User | null };
+
+/** A `FullSector` is a `Sector` with :
+ * - `missions` : {@link Mission[]} the missions assigned to the sector
+ * - `branch` : {@link Branch} the branch the sector belongs to
+ * - `assignedCaregivers` (optional) : {@link Caregiver[]} the caregivers assigned to the sector
+ * - `updatedBy` (optional) : {@link User} the user who updated the sector
+ */
 export type FullSector = Sector & {
   missions: Mission[];
   branch: Branch;
   assignedCaregivers?: Caregiver[];
   updatedBy?: User | null;
 };
+
+/** A `FullMission` is a `Mission` with :
+ * - `sector` : {@link FullSector} the sector the mission belongs to
+ * - `updatedBy` (optional) : {@link User} the user who updated the mission
+ */
 export type FullMission = Mission & {
   sector: Omit<FullSector, 'missions'>;
   updatedBy?: User | null;
 };
+
+/**
+ * A `FullCaregiverSector` is a `CaregiverSector` with :
+ * - `caregiver` (optional) : {@link Caregiver} the caregiver assigned to the sector
+ * - `sector` (optional) : {@link Sector} the sector assigned to the caregiver
+ */
 export type FullCaregiverSector = CaregiverSector & { caregiver?: Caregiver; sector?: Sector };
 
 export type TCalendarOptions = { date: string; recurence: boolean };
