cdc491f179c7207620133a31d79872581393604f
Nicolas Frouin <frouinnicolas@gmail.com>
2025-07-03 15:48:35 +0200

    feat: implement swapAssignmentCaregiver function for caregiver assignment management

--- src/actions/assignments.ts
+++ src/actions/assignments.ts
@@ -1,12 +1,12 @@
 'use server';
 
-import { Caregiver, CaregiverBigWeekType, Sector } from '@/generated/client';
+import { Caregiver, CaregiverBigWeekType, Mission, Sector } from '@/generated/client';
+import prisma from '@/lib/prisma';
 import { getDate, getWeekDays, getWeekNumber, isBigWeek, rand } from '@/lib/utils';
 import { FullAssignment, TCalendarOptions } from '@/types/utils';
-import { getBranchesToMissions } from './data';
-import prisma from '@/lib/prisma';
 import { revalidatePath } from 'next/cache';
 import { writeFile } from 'node:fs';
+import { getBranchesToMissions } from './data';
 
 const DEV = process.env.NODE_ENV === 'development';
 
@@ -36,6 +36,111 @@ const l = DEV
       logger.log(level, ...args);
     }
   : () => {};
+
+export async function swapAssignmentCaregiver(
+  data: {
+    baseCaregiverId: Caregiver['id'] | null;
+    selectedCaregiverId: Caregiver['id'] | null;
+    date: Date;
+    missionId: Mission['id'];
+    color?: string;
+  },
+  userId: Caregiver['id'],
+) {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const result = { success: false, message: '', details: null as any, data: null as any };
+
+  try {
+    const baseAssignmentData = data.baseCaregiverId
+      ? await prisma.assignment.findUnique({
+          where: {
+            date_caregiverId_missionId: {
+              date: data.date,
+              caregiverId: data.baseCaregiverId,
+              missionId: data.missionId,
+            },
+          },
+        })
+      : null;
+    if (data.baseCaregiverId && !baseAssignmentData) {
+      result.message = 'ERR_ASSIGNMENT_NOT_FOUND';
+      return result;
+    }
+
+    if (baseAssignmentData?.caregiverId === data.selectedCaregiverId && data.color) {
+      result.data = await prisma.assignment.update({
+        where: {
+          date_caregiverId_missionId: {
+            date: baseAssignmentData.date,
+            caregiverId: baseAssignmentData.caregiverId,
+            missionId: baseAssignmentData.missionId,
+          },
+        },
+        data: { color: data.color, updatedById: userId },
+      });
+    } else if (data.selectedCaregiverId && !data.baseCaregiverId) {
+      result.data = {};
+      result.data.deleteManyRes = await prisma.assignment.deleteMany({
+        where: {
+          date: data.date,
+          OR: [{ caregiverId: data.selectedCaregiverId }],
+          // @ts-expect-error - forceDelete is custom
+          forceDelete: true,
+        },
+      });
+      result.data.createRes = await prisma.assignment.create({
+        data: {
+          caregiverId: data.selectedCaregiverId,
+          date: new Date(data.date),
+          missionId: data.missionId,
+          color: data.color || null,
+          createdById: userId,
+          updatedById: userId,
+        },
+      });
+    } else if (!data.selectedCaregiverId && data.baseCaregiverId) {
+      result.data = await prisma.assignment.delete({
+        where: {
+          date_caregiverId_missionId: {
+            date: data.date,
+            caregiverId: data.baseCaregiverId,
+            missionId: data.missionId,
+          },
+          OR: [{ archived: true }, { archived: false }],
+        },
+      });
+    } else if (data.selectedCaregiverId && data.baseCaregiverId) {
+      result.data = {};
+      result.data.deleteManyRes = await prisma.assignment.deleteMany({
+        where: {
+          date: data.date,
+          OR: [{ caregiverId: data.selectedCaregiverId }, { caregiverId: data.baseCaregiverId }],
+          // @ts-expect-error - forceDelete is custom
+          forceDelete: true,
+        },
+      });
+      result.data.createRes = await prisma.assignment.create({
+        data: {
+          caregiverId: data.selectedCaregiverId,
+          date: data.date,
+          missionId: data.missionId,
+          color: data.color || data.color,
+          createdById: userId,
+          updatedById: userId,
+        },
+      });
+    } else {
+      result.message = 'ERR_ASSIGNMENT_NO_CAREGIVER';
+      return result;
+    }
+    result.success = true;
+  } catch (error) {
+    result.message = 'ERR_ASSIGNMENT_SWAP_FAILED';
+    result.details = error;
+  }
+
+  return result;
+}
 
 export async function generateWeekCalendar(
   forbiddenSectors: Record<Caregiver['id'], Sector['id'][]>,
@@ -92,8 +197,16 @@ async function calendarGenerator(
 
     for (const branch of bsm) {
       l(1, `Branch : ${branch.name}`);
+      const lastWeekDay = new Date(day);
+      lastWeekDay.setDate(lastWeekDay.getDate() - 7);
       const branchCaregivers = await prisma.caregiver.findMany({
-        include: { assignedSectors: true },
+        include: {
+          assignedSectors: true,
+          assignments: {
+            where: { date: getDate(lastWeekDay) },
+            include: { mission: { select: { sector: { select: { id: true } } } } },
+          },
+        },
         where: {
           active: true,
           branchId: branch.id,
@@ -101,22 +214,35 @@ async function calendarGenerator(
         },
       });
 
-      const caregiversToAssign = [...branchCaregivers];
+      const caregiversToAssign = [...branchCaregivers].sort(() => Math.random() - 0.5);
 
       /* Mandatory Missions */
       l(2, `Mandatory missions`);
       for (const sector of branch.sectors) {
         const mandatoryMissions = sector.missions.filter((m) => m.min > 0);
+
+        const eligibleCaregivers = caregiversToAssign.reduce(
+          (acc, c) => {
+            const caregiverCurrentSector = c.assignedSectors.find(
+              (s) => s.sectorId === sector.id && !forbiddenSectors[c.id]?.includes(s.sectorId),
+            );
+
+            if (
+              caregiverCurrentSector /* &&
+              (calendarOptions.recurrence || c.assignedSectors.length === 1) */
+            ) {
+              acc.push(c);
+            }
+            return acc;
+          },
+          [] as typeof branchCaregivers,
+        );
+
         l(3, `Sector : ${sector.name} (${mandatoryMissions.length} missions)`);
         for (const mission of mandatoryMissions) {
           l(4, `Mission : ${mission.name} (min: ${mission.min})`);
 
           l(5, 'Caregivers to assign:', caregiversToAssign.length);
-          const eligibleCaregivers = caregiversToAssign.filter(
-            (c) =>
-              c.assignedSectors.some((s) => s.sectorId === sector.id) &&
-              !forbiddenSectors[c.id]?.includes(sector.id),
-          );
 
           const assignedCaregivers = eligibleCaregivers.splice(
             rand(0, eligibleCaregivers.length - mission.min),
@@ -167,16 +293,29 @@ async function calendarGenerator(
       l(2, `Optional missions`);
       for (const sector of branch.sectors) {
         const optionalMissions = sector.missions.filter((m) => m.min <= 0);
+
+        const eligibleCaregivers = caregiversToAssign.reduce(
+          (acc, c) => {
+            const caregiverCurrentSector = c.assignedSectors.find(
+              (s) => s.sectorId === sector.id && !forbiddenSectors[c.id]?.includes(s.sectorId),
+            );
+
+            if (
+              caregiverCurrentSector /* &&
+              (!calendarOptions.recurrence || c.assignedSectors.length === 1) */
+            ) {
+              acc.push(c);
+            }
+            return acc;
+          },
+          [] as typeof branchCaregivers,
+        );
+
         l(3, `Sector : ${sector.name} (${optionalMissions.length} missions)`);
         for (const mission of optionalMissions) {
           l(4, `Mission : ${mission.name} (max: ${mission.max})`);
 
           l(5, 'Caregivers to assign:', caregiversToAssign.length);
-          const eligibleCaregivers = caregiversToAssign.filter(
-            (c) =>
-              c.assignedSectors.some((s) => s.sectorId === sector.id) &&
-              !forbiddenSectors[c.id]?.includes(sector.id),
-          );
 
           const assignedCaregivers = eligibleCaregivers.splice(
             rand(0, eligibleCaregivers.length - mission.max),
